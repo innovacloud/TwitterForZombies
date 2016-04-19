44ffa6461f8d465b0a2c9a3caf685025e5dffe5d
Ramses Tovar <ramses.tovar@gmail.com>
2016-04-19 16:34:39 -0430

    Set timezone to Caracas

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,9 @@ module TwitterForZombies
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Set timezone to Caracas
+    config.time_zone = 'Caracas'
+    
   end
 end
